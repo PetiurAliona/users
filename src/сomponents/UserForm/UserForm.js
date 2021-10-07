879782bd80a1cb60b5c3523edcb1c5fa96a81c45
PetiurAliona <alena.petyur@gmail.com>
2021-10-08 00:57:35 +0300

    +validate Form

--- src/сomponents/UserForm/UserForm.js
+++ src/сomponents/UserForm/UserForm.js
@@ -5,24 +5,59 @@ import { addUser } from "../Utills/apiServices"
 import styles from "./UserForm.module.css"
 
 const UserForm = ({ onSubmit, setCloseModal }) => {
-  const [firstName, setFirstName] = useState("")
-  const [lastName, setLastName] = useState("")
-  const [email, setEmail] = useState("")
-  const [title, setTitle] = useState("")
+  const [title, setTitle] = useState("mr")
   const [picture, setPicture] = useState("")
+
+  const [user, setUser] = useState({ firstName: "", lastName: "", email: "" })
+
+  const [error, setError] = useState({ firstName: null, lastName: null, email: null })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setUser((prev) => ({ ...prev, [name]: value }))
+    setError((prev) => ({ ...prev, [name]: null }))
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault()
+    let isError = false
+    Object.entries(user).forEach(([key, value]) => {
+      if (!value) {
+        const mes = "This field is required"
+        isError = true
+        setError((prev) => ({ ...prev, [key]: mes }))
+      }
+      if (key !== "email") {
+        if (value.length < 3 || value.length > 50) {
+          const mes = "Field must be at least 3 long and not exceed 50 "
+          isError = true
+          setError((prev) => ({ ...prev, [key]: mes }))
+        }
+      }
+      if (key === "email") {
+        const reg =
+          /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+        if (!value.match(reg)) {
+          const mes = "Email is not valid"
+          isError = true
+          setError((prev) => ({ ...prev, [key]: mes }))
+        }
+      }
+    })
+
+    if (isError) return
 
     try {
-      const response = await addUser({ title, firstName, lastName, email, picture })
+      const response = await addUser({ ...user, title, picture })
       onSubmit(response)
-      setFirstName("")
-      setLastName("")
-      setEmail("")
+      setTitle("")
+      setPicture("")
       setCloseModal()
-    } catch (error) {
-      console.log(error)
+    } catch (e) {
+      Object.entries(e.data).forEach(([key, value]) => {
+        console.log(key, value)
+        setError((prev) => ({ ...prev, [key]: value }))
+      })
     }
   }
 
@@ -31,7 +66,6 @@ const UserForm = ({ onSubmit, setCloseModal }) => {
       <label>
         Title
         <select name="title" defaultValue={title} onChange={(e) => setTitle(e.target.value)}>
-          <option></option>
           <option value="mr">mr</option>
           <option value="mrs">mrs</option>
           <option value="ms">ms</option>
@@ -41,39 +75,39 @@ const UserForm = ({ onSubmit, setCloseModal }) => {
       </label>
 
       <Input
-        name={firstName}
+        name="firstName"
         label="First Name"
-        onChange={(e) => setFirstName(e.currentTarget.value)}
-        value={firstName}
-        isError
+        onChange={handleChange}
+        value={user.firstName}
+        isError={error.firstName}
         className=""
         inputId="firstName"
       />
       <Input
-        name={lastName}
+        name="lastName"
         label="Last Name"
-        onChange={(e) => setLastName(e.currentTarget.value)}
-        value={lastName}
-        isError
+        onChange={handleChange}
+        value={user.lastName}
+        isError={error.lastName}
         className=""
         inputId="lastName"
       />
       <Input
-        name={email}
+        name="email"
         label="Email"
-        onChange={(e) => setEmail(e.currentTarget.value)}
-        value={email}
-        isError
+        onChange={handleChange}
+        value={user.email}
+        isError={error.email}
         className=""
         inputId="email"
       />
 
       <Input
-        name={picture}
+        name="picture"
         label="Avatar"
         onChange={(e) => setPicture(e.currentTarget.value)}
         value={picture}
-        isError
+        isError={null}
         className=""
         inputId="picture"
       />
--- src/сomponents/Utills/apiServices.js
+++ src/сomponents/Utills/apiServices.js
@@ -1,12 +1,17 @@
-export async function getUsers(url = "https://dummyapi.io/data/v1/user") {
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "app-id": "615d85b1eb3d1fe948797d59",
-    },
-  })
-  return response.json()
+export async function getUsers() {
+  try {
+    const baseUrl = "https://dummyapi.io/data/v1/user"
+    const response = await fetch(baseUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "app-id": "615d85b1eb3d1fe948797d59",
+      },
+    })
+    return response.json()
+  } catch (error) {
+    throw new Error(error)
+  }
 }
 
 export async function addUser(data) {
@@ -19,7 +24,6 @@ export async function addUser(data) {
     },
     body: JSON.stringify(data),
   })
-  return response.json()
+  console.log(response)
+  return response.ok ? await response.json() : Promise.reject(await response.json())
 }
-
-getUsers().then(console.log)
